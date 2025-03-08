73aa2e755c37a3bc059189b620ce99227b8aa7c4
Heshan Padmasiri <hpheshan@gmail.com>
2025-03-08 08:15:07 +0530

    Implement player

--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,93 @@
-// raylib-zig (c) Nikolas Wipper 2023
-
 const rl = @import("raylib");
 
+const Player = struct { position: rl.Vector2, size: rl.Vector2, speed: rl.Vector2 };
+
+const World = struct { player: Player };
+
+const playerSize = rl.Vector2.init(200, 25);
+
+const screenWidth = 800;
+const screenHeight = 450;
+
 pub fn main() anyerror!void {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const screenWidth = 800;
-    const screenHeight = 450;
+    var world = initWorld(screenHeight, screenWidth);
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
-    defer rl.closeWindow(); // Close window and OpenGL context
+    rl.initWindow(screenWidth, screenHeight, "Zig BreakOut");
+    defer rl.closeWindow();
 
-    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    rl.setTargetFPS(60);
+    while (!rl.windowShouldClose()) {
+        if (rl.isKeyDown(rl.KeyboardKey.l)) {
+            move_player_right(&world.player);
+        } else if (rl.isKeyDown(rl.KeyboardKey.h)) {
+            move_player_left(&world.player);
+        } else {
+            stop_player(&world.player);
+        }
+        const dt = rl.getFrameTime();
+        update_world(&world, dt);
 
-    // Main game loop
-    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.white);
-
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
-        //----------------------------------------------------------------------------------
+        draw_player(world.player);
     }
+}
+
+fn move_player_left(player: *Player) void {
+    player.speed.x = -200;
+}
+
+fn move_player_right(player: *Player) void {
+    player.speed.x = 200;
+}
+
+fn stop_player(player: *Player) void {
+    player.speed.x = 0;
+}
+
+fn update_world(world: *World, dt: f32) void {
+    // TODO: do any collision detection logic and update the speed of stuff
+    handle_player_boundaries(&world.player, screenWidth);
+    update_player(&world.player, dt);
+}
+
+fn handle_player_boundaries(player: *Player, width: i32) void {
+    if (player.position.x <= 0 and player.speed.x < 0) {
+        player.position.x = 0;
+        player.speed.x = 0;
+    } else if (player.position.x + player.size.x >= @as(f32, @floatFromInt(width)) and player.speed.x > 0) {
+        player.position.x = @as(f32, @floatFromInt(width)) - player.size.x;
+        player.speed.x = 0;
+    }
+}
+
+fn update_player(player: *Player, dt: f32) void {
+    player.position.x += player.speed.x * dt;
+    player.position.y += player.speed.y * dt;
+}
+
+fn initWorld(height: i32, width: i32) World {
+    const playerXOffset = playerSize.x / 2;
+    const playerYOffset = playerSize.y / 2;
+    return World{ .player = init_player((@as(f32, @floatFromInt(width)) / 2.0) - playerXOffset, (@as(f32, @floatFromInt(height)) / 1.25) - playerYOffset) };
+}
+
+fn init_player(x: f32, y: f32) Player {
+    const position = rl.Vector2.init(x, y);
+    debug_print_vec("start position", position);
+    const size = playerSize;
+    const speed = rl.Vector2.zero();
+    return Player{ .position = position, .size = size, .speed = speed };
+}
+
+fn debug_print_vec(name: []const u8, vec: rl.Vector2) void {
+    const x = vec.x;
+    const y = vec.y;
+    @import("std").debug.print("{s} x: {d}, y: {d}\n", .{ name, x, y });
+}
+
+fn draw_player(player: Player) void {
+    rl.drawRectangleV(player.position, player.size, rl.Color.red);
 }
