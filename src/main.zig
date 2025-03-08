1f8a73fa1d0d162fd0f79685a5a34ae418b5aadc
Heshan Padmasiri <hpheshan@gmail.com>
2025-03-08 08:33:53 +0530

    Implement ball

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,21 @@
 const rl = @import("raylib");
 
-const Player = struct { position: rl.Vector2, size: rl.Vector2, speed: rl.Vector2 };
+// FIXME: try to use a packed struct and treat all objects as same
+// -- this will require Vecs to be pointers
+const Player = struct { position: rl.Vector2, speed: rl.Vector2, size: rl.Vector2 };
+const Ball = struct { position: rl.Vector2, speed: rl.Vector2, radius: f32 };
 
-const World = struct { player: Player };
+const World = struct { player: Player, ball: Ball };
 
 const playerSize = rl.Vector2.init(200, 25);
 
 const screenWidth = 800;
 const screenHeight = 450;
 
+const ballRadius = 10;
+
 pub fn main() anyerror!void {
-    var world = initWorld(screenHeight, screenWidth);
+    var world = init_world(screenHeight, screenWidth);
 
     rl.initWindow(screenWidth, screenHeight, "Zig BreakOut");
     defer rl.closeWindow();
@@ -32,6 +37,7 @@ pub fn main() anyerror!void {
 
         rl.clearBackground(rl.Color.white);
         draw_player(world.player);
+        draw_ball(world.ball);
     }
 }
 
@@ -51,6 +57,7 @@ fn update_world(world: *World, dt: f32) void {
     // TODO: do any collision detection logic and update the speed of stuff
     handle_player_boundaries(&world.player, screenWidth);
     update_player(&world.player, dt);
+    update_ball(&world.ball, dt);
 }
 
 fn handle_player_boundaries(player: *Player, width: i32) void {
@@ -68,10 +75,23 @@ fn update_player(player: *Player, dt: f32) void {
     player.position.y += player.speed.y * dt;
 }
 
-fn initWorld(height: i32, width: i32) World {
+fn update_ball(ball: *Ball, dt: f32) void {
+    ball.position.x += ball.speed.x * dt;
+    ball.position.y += ball.speed.y * dt;
+}
+
+fn init_world(height: i32, width: i32) World {
     const playerXOffset = playerSize.x / 2;
     const playerYOffset = playerSize.y / 2;
-    return World{ .player = init_player((@as(f32, @floatFromInt(width)) / 2.0) - playerXOffset, (@as(f32, @floatFromInt(height)) / 1.25) - playerYOffset) };
+    return World{ .player = init_player((@as(f32, @floatFromInt(width)) / 2.0) - playerXOffset, (@as(f32, @floatFromInt(height)) / 1.25) - playerYOffset), .ball = init_ball((@as(f32, @floatFromInt(width)) / 2.0) - ballRadius, (@as(f32, @floatFromInt(height)) / 2) - ballRadius) };
+}
+
+fn init_ball(x: f32, y: f32) Ball {
+    const position = rl.Vector2.init(x, y);
+    // FIXME:
+    const speed = rl.Vector2.init(0, 100);
+    const radius = @as(f32, ballRadius);
+    return Ball{ .position = position, .speed = speed, .radius = radius };
 }
 
 fn init_player(x: f32, y: f32) Player {
@@ -90,4 +110,8 @@ fn debug_print_vec(name: []const u8, vec: rl.Vector2) void {
 
 fn draw_player(player: Player) void {
     rl.drawRectangleV(player.position, player.size, rl.Color.red);
+}
+
+fn draw_ball(ball: Ball) void {
+    rl.drawCircleV(ball.position, ball.radius, rl.Color.blue);
 }
